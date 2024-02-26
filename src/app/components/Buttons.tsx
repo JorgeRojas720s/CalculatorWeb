22380098499ad44c3843d79b0b433ed2d9d1fcc3
JorgeRojas720s <jitorojas37@gmail.com>
2024-02-25 23:28:02 -0600

    Oeration add finished

--- src/app/components/Buttons.tsx
+++ src/app/components/Buttons.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 type ButtonParams = {
   number: string;
+  key: number
   onClick: (params: any) => void;
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,64 @@
-'use client';
-import React from 'react';
-import Buttons from './components/Buttons';
-import "./style.css"
+"use client";
+import * as React from "react";
+import Buttons from "./components/Buttons";
+import "./style.css";
 
 export default function Home() {
+  const buttonsCalculator = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "+",
+    "-",
+    "x",
+    "/",
+    "=",
+  ];
 
-  const buttonsCalculator = ["1","2","3","4","5","6","7","8","9","0","+","-","x","/","="];
+  const [total, setTotal] = React.useState<string>("");
 
   function clickButton(value: string) {
+    let result;
+    let secondNumber = "";
+    let operator;
+    let firstNumbeer = "";
+
     console.log("clickeadoooooo  ", value);
+    setTotal(total + value);
+
+    total.split("+");
+
   }
 
   function fillButtons(buttonsCalculator: string[]): JSX.Element[] {
-    return buttonsCalculator.map((button) => (
-      <Buttons number={button} onClick={() => clickButton(button)}/>
+    return buttonsCalculator.map((button, key) => (
+      <Buttons number={button} key={key} onClick={() => clickButton(button)} />
     ));
   }
 
-
   return (
     <main>
-      <div className="title">
-        <h1>Calculator</h1>
-      </div>
-      <div className="buttons-container">
-        <div className="total-container">
-          <h2>Total</h2>
+      <section>
+        <div className="title">
+          <h1>Calculator</h1>
         </div>
-        {fillButtons(buttonsCalculator)}
-      </div>
+      </section>
+      <section>
+        <div className="buttons-container">
+          <div className="total-container">
+            <h2>Total</h2>
+            <div className="screen">{total}</div>
+          </div>
+          {fillButtons(buttonsCalculator)}
+        </div>
+      </section>
     </main>
   );
 }
